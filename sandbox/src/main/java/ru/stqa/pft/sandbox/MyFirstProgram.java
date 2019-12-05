3dfa00afc88f0b85897e72dc61a2de9167ef18e2
mabur <mabur@bk.ru>
2019-12-05 23:05:54 +0400

    добавлен цикл

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+
+	public static void main(String[] args){
+		for(int i=0; i<10; i++)
+			System.out.println("hello, world");
+
+	}
+	}
+	
