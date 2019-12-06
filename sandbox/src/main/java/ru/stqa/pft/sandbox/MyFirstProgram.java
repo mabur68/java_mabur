ce9cdf3c3029218c39ffda09e86acf82ece66838
mabur <mabur@bk.ru>
2019-12-06 23:04:54 +0400

    добавлено несколько ыункций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,14 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
-	public static void main(String[] args){
-		for(int i=0; i<10; i++) {
-			if(i%2!=0){
-                System.out.println("hai");
-			    continue;
-            }
-			System.out.println("hello, world");
-		}
-	}
-	}
+  public static void main(String[] args) {
+    String name;
+    double l = 5;
+    double a = 8;
+    double b = 7;
+    System.out.println("площадь прямоугольника со сторонами 8 и 7 равна " + area(a, b));
+    System.out.println("площадь квадрата со стороной 5, равна " + area(l));
+    helo("vasja");
+  }
+
+  public static void helo(String name) {
+    System.out.println("Hello, " + name);
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+}
 	
