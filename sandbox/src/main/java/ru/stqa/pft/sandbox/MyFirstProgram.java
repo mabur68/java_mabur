f7a4cb74519f8f2644c9de0ff7057439e640fa2a
mabur <mabur@bk.ru>
2019-12-06 21:52:03 +0400

    ломаем дальше

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,9 +4,13 @@ public class MyFirstProgram {
 
 
 	public static void main(String[] args){
-		for(int i=0; i<10; i++)
+		for(int i=0; i<10; i++) {
+			if(i%2!=0){
+                System.out.println("hai");
+			    continue;
+            }
 			System.out.println("hello, world");
-
+		}
 	}
 	}
 	
